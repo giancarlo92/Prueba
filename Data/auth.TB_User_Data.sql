27cf908fe91d9aaa6506f0e91f90e8482387cfd1
Giancarlo <giancarlo.zevallos.92@gmail.com>
2020-01-10 00:09:43 -0500

    Conectando TB_User data

---
+++ Data/auth.TB_User_Data.sql
@@ -0,0 +1,1 @@
+INSERT INTO [auth].[TB_User] ([IDUser], [Username], [Password], [Nickname]) VALUES ('7d9b4666-74f6-4833-b2a6-e38fbccb7d57', N'pruebas@bancoripley.com.pe', N'p455w0rd', N'Billy Arredondo')
