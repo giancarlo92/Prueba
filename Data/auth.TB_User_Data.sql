156d70fc77b60604102dcc4ce7ab8dfe980a4da2
Giancarlo <giancarlo.zevallos.92@gmail.com>
2020-01-10 00:24:16 -0500

    add new user

--- Data/auth.TB_User_Data.sql
+++ Data/auth.TB_User_Data.sql
@@ -1,2 +1,3 @@
-INSERT INTO [auth].[TB_User] ([IDUser], [Username], [Password], [Nickname]) VALUES ('7d9b4666-74f6-4833-b2a6-e38fbccb7d58', N'giancarlo@gmail.com', N'123456', N'Giancarlo Zevallos')
+INSERT INTO [auth].[TB_User] ([IDUser], [Username], [Password], [Nickname]) VALUES ('7d9b4666-74f6-4833-b2a6-e38fbccb7d59', N'alex@gmail.com', N'123456', N'Alex Villalobos')
 INSERT INTO [auth].[TB_User] ([IDUser], [Username], [Password], [Nickname]) VALUES ('7d9b4666-74f6-4833-b2a6-e38fbccb7d57', N'pruebas@bancoripley.com.pe', N'p455w0rd', N'Billy Arredondo')
+INSERT INTO [auth].[TB_User] ([IDUser], [Username], [Password], [Nickname]) VALUES ('7d9b4666-74f6-4833-b2a6-e38fbccb7d58', N'giancarlo@gmail.com', N'123456', N'Giancarlo Zevallos')
